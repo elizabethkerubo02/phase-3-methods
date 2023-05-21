7cf8acb6f1792fb98303762d94d496b54c45c1b8
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-21 20:04:44 +0300

    completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+greet_programmer()
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+
+def greetWithDefault name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Sunny")
+
+def add num1, num2
+    num1 + num2
+
+end    
+
+def halve number
+     if number.class != Integer
+        return nil
+        
+     end
+     number /2
+end
+ 
+
+
